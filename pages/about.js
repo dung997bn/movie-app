90b3c11c1d06baf104da2661a2ef06f995c24713
dung997bn <phaichisophan@gmail.com>
2020-07-09 22:01:51 +0700

    End of App Container

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const About = () => {
+const About = (props) => {
   return (
     <div>
       <h1>Hello ablout</h1>
+      <h2>{props.a}</h2>
     </div>
   );
+};
+
+About.getInitialProps = () => {
+  let a = 1;
+  return { a };
 };
 
 export default About;
